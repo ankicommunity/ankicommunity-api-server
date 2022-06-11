5f73006aef1fef4563fe11b8c8b209acf9199390
Vikash Kothary <kothary.vikash@gmail.com>
2022-06-11 19:30:45 +0100

    chore: Allow setting the port via environment variables

--- scripts/rundevserver.sh
+++ scripts/rundevserver.sh
@@ -4,5 +4,6 @@ source scripts/runsetup.sh
 
 export DJANKISERV_DEBUG=true
 export DJANKISERV_ALLOWED_HOSTS='*'
+[[ ! -z "${DJANKISERV_PORT}" ]] || DJANKISERV_PORT=5000
 
-python3 -m djankiserv_cli runserver 0.0.0.0:8002
+python3 -m djankiserv_cli runserver 0.0.0.0:${DJANKISERV_PORT}
