336dede7f5bcc38d489200962156d3269fa9b4ec
Vikash Kothary <kothary.vikash@gmail.com>
2022-06-11 16:45:23 +0100

    chore: Refactor runmanage to use environment variables

--- scripts/runmanage.sh
+++ scripts/runmanage.sh
@@ -2,4 +2,10 @@
 
 source scripts/runsetup.sh
 
-python src/manage.py $@
+
+[[ ! -z "${DJANGO_MANAGE}" ]] || DJANGO_MANAGE="${PYTHON} src/manage.py"
+# [[ ! -z "${DJANGO_MANAGE_OPTS}" ]] || DJANGO_MANAGE_OPTS=
+[[ ! -z "${DJANGO_MANAGE_CMD}" ]] || DJANGO_MANAGE_CMD=$@
+# [[ ! -z "${DJANGO_MANAGE_ARGS}" ]] || DJANGO_MANAGE_CMD=$@
+
+${DJANGO_MANAGE} ${DJANGO_MANAGE_CMD}
