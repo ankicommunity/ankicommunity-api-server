e9911c4d1507ccb28a931cf7cefa1d0bf2f594d9
Vikash Kothary <kothary.vikash@gmail.com>
2022-06-18 03:49:58 +0100

    refactor: Remove unused import

--- src/djankiserv_app/urls.py
+++ src/djankiserv_app/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from django.views.generic.base import RedirectView
 
 from djankiserv_app import views
